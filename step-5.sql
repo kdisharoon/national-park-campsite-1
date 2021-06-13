80685d3f2c89bb6a0f0eebe736d1282044f9cf6e
James Cope <james.cope26@gmail.com>
2021-06-13 13:10:27 -0400

    1 thru 5 complete JC

--- step-5.sql
+++ step-5.sql
@@ -10,12 +10,18 @@
   ------------------------------
 */
 
+insert into park (name, location, establish_date, area, visitors, description) 
+values ('Ohiopyle State Park', 'Pennsylvania', '1965-01-01', '19052', '1000000', ' Ohiopyle State Park is a Pennsylvania state park on 19,052 acres in Dunbar, Henry Clay and Stewart Townships, Fayette County, Pennsylvania in the United States. The focal point of the park is the more than 14 miles of the Youghiogheny River Gorge that passes through the park.')
+;
 
 /*
   STEP TWO: You just found out that there was an error with the park data. Please update the park visitors to 1.5 million anually.
 
 */
 
+update park set visitors = 1500000
+where name like 'Ohiopyle %'
+;
 
 /*
  STEP THREE: Insert new campground with the following data:
@@ -28,6 +34,9 @@
   ------------------------------------------------------------
 */
 
+insert into campground (park_id, name, open_from_mm, open_to_mm, daily_fee) 
+values ('4', 'Youghiogheny', '01', '12', '95.00')
+;
 
 /*
  STEP FOUR: Insert 3 new sites with the following data:
@@ -41,6 +50,9 @@
 
 */
 
+insert into site (site_number, campground_id)
+values (623,8), (624,8), (625,8)
+;
 
 /*
  STEP FIVE: Insert 3 reservations, 1 for each site with the following data:
@@ -52,15 +64,29 @@
 
 */
 
+insert into reservation (site_id, name, from_date, to_date)
+values (623, 'Wayne Family', '2021-06-23', '2021-07-03'), 
+       (624, 'Parker Family', '2021-06-24', '2021-07-03'),
+       (625, 'Kent Family', '2021-06-25', '2021-07-03')
+;
+
 
 /*
  STEP SIX: The Wayne Family called and asked if they could change their reservation to today. Update the from_date to today and the to_date to a week from today.
 
  */
 
+update reservation 
+set from_date = '2021-06-13', to_date = '2021-06-20'
+where name = 'Wayne Family'
+;
 
 /*
  STEP SEVEN: The Kent family called and they would like to cancel their reservation. Delete the reservation for Kent Family.
 
 */
+
+delete from reservation
+where name = 'Kent Family'
+;
 
