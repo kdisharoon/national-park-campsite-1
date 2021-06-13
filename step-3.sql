6f84f01260f30423d1d9490ba2ae8a2a8ac2b421
James Cope <james.cope26@gmail.com>
2021-06-13 18:58:08 -0400

    Dish step 3-5, Cope jc3-5

--- step-3.sql
+++ step-3.sql
@@ -1,93 +1,47 @@
 -- CAMPGROUND TABLE
 -----------------------------------------------
 
-select name, daily_fee
-from campground;
 -- select name and daily fee of all campgrounds
 
-select name, daily_fee
-from campground
-order by name;
 -- select name and daily fee of all campgrounds ordered by campground name
 
-select *
-from campground
-where daily_fee < '100';
 -- select name, open from month, open to month, and daily fee of the campgrounds where daily fee is less than $100
 
-select name, daily_fee
-from campground
-where open_from_mm = '01' and open_to_mm = '12';
 -- select name and daily fee of the campgrounds where the campground is open all year long
 
 
 -- PARK TABLE
 -----------------------------------------------
-select name, description
-from park p
-order by p.establish_date desc;
+
 -- select name and description of all parks order by established date in descending order
 
-select name, description
-from park
-where location = 'Ohio';
 -- select name and description of all parks in Ohio
 
-select name, description
-from park
-where location != 'Ohio';
 -- select name and description of all parks NOT in Ohio
 
-select sum(visitors) as total_visitors
-from park;
 -- select the total number of visitors for all parks
 
-select avg(visitors) as avg_visitors
-from park;
 -- select the average number of visitors for all parks
 
 
 -- SITE TABLE
 -----------------------------------------------
 
-select *
-from site
-where utilities
-is true
-order by max_rv_length desc;
 -- select all columns from site where utilities is true and order by max RV length with the largest coming first
 
-select sum(site_number) as total_util_sites
-from site
-where utilities
-is true;
 -- select total number of sites that have utilities hook up
 
 
 -- RESERVATION TABLE
 -----------------------------------------------
-select reservation_id, site_id, name, from_date, to_date
-from reservation
-where from_date between '2021-06-01' and '2021-06-30';
+
 -- select reservation id, site id, name, from date, to date of the reservations where the checkin date is between the first and last day of the current month (hard coded month is ok)
 
-select *
-from reservation
-where name like '% Reservation';
 -- select all columns from reservation where name includes 'Reservation'
 
-select sum(reservation_id) as total_reservations
-from reservation;
 -- select the total number of reservations in the reservation table
 
-select reservation_id, site_id, name
-from reservation
-where site_id in (9, 20, 24, 45, 46);
 -- select reservation id, site id, name of the reservations where site id is in the list 9, 20, 24, 45, 46
 
-select from_date, sum(reservation_id) as number_of_res
-from reservation r
-group by r.from_date
-order by r.from_date asc;
 -- select the date and number of reservations for each date orderd by from_date in ascending order
 
--- step-4.sql
+++ step-4.sql
@@ -1,30 +1,12 @@
 -- select the park name, campground name, open_from_mm, open_to_mm & daily_fee ordered by park name and then campground name
-select park.name,c.name, open_from_mm, open_to_mm, daily_fee
-from campground c
-join park on park.park_id = c.park_id
-order by park.name, c.name;
+
 
 -- select the park name and the total number of campgrounds for each park ordered by park name
-select park.name, sum(c.campground_id)
-from park
-join campground c on c.park_id = park.park_id
-group by park.name
-order by park.name;
+
 
 
 -- select the park name, campground name, site number, max occupancy, accessible, max rv length, utilities where the campground name is 'Blackwoods'
-select p.name, c.name, s.site_number, s.max_occupancy, s.accessible, s.max_rv_length, s.utilities
-from park p
-join campground c using (park_id)
-join site s using (campground_id)
-where c.name = 'Blackwoods';
 
-select p.name, c.name, count(s.site_number) as number_of_sites
-from park p
-join campground c using (park_id)
-join site s  using (campground_id)
-group by  p.name, c.name
-order by p.name;
 /*
   select park name, campground, total number of sites (column alias 'number_of_sites') ordered by park
   -------------------------------------------------
@@ -38,19 +20,11 @@ order by p.name;
   -------------------------------------------------
 */
 
-select site_number, r.name, r.from_date, r.to_date
-from reservation r
-join site using (site_id)
-order by r.from_date;
+
 -- select site number, reservation name, reservation from and to date ordered by reservation from date
 
 
-select c.name, count(reservation_id) as total_reservations
-from campground c
-join site using (campground_id)
-join reservation using (site_id)
-group by c.name
-order by total_reservations desc;
+
 /*
   select campground name, total number of reservations for each campground ordered by total reservations desc
   -------------------------------------------------
